3987374e51d1fd857dfd0bdca68e70a25bf5caf8
Chris McComb <ccmcc2012@gmail.com>
2022-11-29 18:46:01 -0500

    Update fix-hf-gradio.js

--- assets/javascript/fix-hf-gradio.js
+++ assets/javascript/fix-hf-gradio.js
@@ -1,1 +1,10 @@
-document.getElementsByTagName("gradio-app")[0].wrapper.classList.remove("dark")
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// Usage!
+sleep(5000).then(() => {
+  document.getElementsByTagName("gradio-app")[0].wrapper.classList.remove("dark")
+});
+
